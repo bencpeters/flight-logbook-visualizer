b811ac268525eb1d613ccacf789ea124d15074cf
Ben Peters <bnpeters@amazon.com>
2026-05-26 11:01:48 -0700

    Restore FAA chart tiles with crossOrigin fix
    The ArcGIS-hosted FAA tiles DO support CORS when the browser sends an
    Origin header. Adding crossOrigin:'anonymous' to tile layer options
    triggers this. Now have VFR Sectionals, VFR Terminal, and IFR Low
    Enroute charts as basemap options.

--- app.js
+++ app.js
@@ -101,14 +101,25 @@
             maxZoom: 17
         });
 
-        const esriTopoLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-            attribution: '&copy; Esri',
-            maxZoom: 19
+        const vfrSectionalLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/VFR_Sectional/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'FAA Aeronautical Information Services',
+            maxZoom: 12,
+            minZoom: 8,
+            crossOrigin: 'anonymous'
         });
 
-        const cartoLightLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; CARTO',
-            maxZoom: 20
+        const vfrTerminalLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/VFR_Terminal/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'FAA Aeronautical Information Services',
+            maxZoom: 14,
+            minZoom: 10,
+            crossOrigin: 'anonymous'
+        });
+
+        const ifrLowLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/IFR_AreaLow/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'FAA Aeronautical Information Services',
+            maxZoom: 12,
+            minZoom: 6,
+            crossOrigin: 'anonymous'
         });
 
         osmLayer.addTo(map);
@@ -117,8 +128,9 @@
             'Roads (OSM)': osmLayer,
             'Satellite': satelliteLayer,
             'Terrain': terrainLayer,
-            'Esri Topo': esriTopoLayer,
-            'Light (CARTO)': cartoLightLayer
+            'VFR Sectionals': vfrSectionalLayer,
+            'VFR Terminal': vfrTerminalLayer,
+            'IFR Low Enroute': ifrLowLayer
         }, null, { position: 'topleft' }).addTo(map);
 
         routeLayer = L.layerGroup().addTo(map);
