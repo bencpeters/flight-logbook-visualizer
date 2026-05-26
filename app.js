96d75e337181f04bc5e63e852ba7fd71b39ad6cb
Ben Peters <bnpeters@amazon.com>
2026-05-26 11:25:31 -0700

    Revert "Remove minZoom from FAA tile layers to fix disabled radio buttons"
    This reverts commit ec85fbb17510086038e4ffdfa02c8ae93b9130d7.

--- app.js
+++ app.js
@@ -103,17 +103,23 @@
 
         const vfrSectionalLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/VFR_Sectional/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'FAA Aeronautical Information Services',
-            maxZoom: 12
+            maxZoom: 12,
+            minZoom: 8,
+            crossOrigin: 'anonymous'
         });
 
         const vfrTerminalLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/VFR_Terminal/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'FAA Aeronautical Information Services',
-            maxZoom: 14
+            maxZoom: 14,
+            minZoom: 10,
+            crossOrigin: 'anonymous'
         });
 
         const ifrLowLayer = L.tileLayer('https://tiles.arcgis.com/tiles/ssFJjBXIUyZDrSYZ/arcgis/rest/services/IFR_AreaLow/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'FAA Aeronautical Information Services',
-            maxZoom: 12
+            maxZoom: 12,
+            minZoom: 6,
+            crossOrigin: 'anonymous'
         });
 
         osmLayer.addTo(map);
